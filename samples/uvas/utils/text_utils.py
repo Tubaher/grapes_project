5bafb671919d4e4eca25854860961fcf4239141d
ZosoV <hoskarg9619@gmail.com>
2021-01-27 20:51:28 -0500

    Some corrections in splash line cross

--- samples/uvas/utils/text_utils.py
+++ samples/uvas/utils/text_utils.py
@@ -2,10 +2,10 @@ import cv2
 
 def draw_text_area(image, string, window_size, location = "left-top", margin = 5, padding = 5, alpha = 0.65,
   font = cv2.FONT_HERSHEY_SIMPLEX, thickness = 1, baseline = 2, rectangle_color = [255, 255, 255], text_color = [0,0,0]):
-    print("location: ", location)
     window_w, window_h  = window_size
     text_w, text_h = cv2.getTextSize(string, font, thickness, baseline)[0]
 
+    #Start the bbox from the left-top corner
     box_xmin = 0
     box_xmax = text_w + 2 * padding
     box_ymin = 0
@@ -40,20 +40,3 @@ def draw_text_area(image, string, window_size, location = "left-top", margin = 5
     image = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
 
     return image
-
-
-# # img = cv2.imread("picture.jpg")
-# # h, w, c = img.shape
-# # print(w,h)
-
-# # # img = draw_text_area(img, "Hola", (w,h), location="left-top")
-# # # img = draw_text_area(img, "Hola", (w,h), location="left-bottom")
-# # # img = draw_text_area(img, "Hola", (w,h), location="rigth-bottom")
-# # img = draw_text_area(img, "Hola", (w,h), location="rigth-top")
-
-# # cv2.imwrite("test.jpg",img)
-# # #cv2.imshow("window_name", img) 
-  
-# # #waits for user to press any key  
-# # #(this is necessary to avoid Python kernel form crashing) 
-# # #cv2.waitKey(0) 
